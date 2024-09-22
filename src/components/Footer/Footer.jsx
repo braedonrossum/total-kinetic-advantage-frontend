35e734196a8c0b7686e4c9e55ae0820ef5407e31
Braedon <braedon.rossum@gmail.com>
2024-09-21 20:48:09 -0600

    change footer styling, add more links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,28 @@
+import { Link } from "react-router-dom";
 import "./Footer.scss"
 
 function Footer() {
   return (
     <div className='footer'>
-      <h3 className='footer__title'>Total Kinetic Advantage &copy;2024</h3>
       <section>
-        <h4>Contact</h4>
         <ul className='footer__list'>
-          <li className='footer__list-item'>
-            Braedon Rossum
-          </li>
-          <li onClick={(e) => {window.location.href ='mailto:tka@gmail.com';}} className='footer__list-item'>
+        <p>Connect With Us</p>
+          <li onClick={(e) => {window.location.href ='mailto:braedon.tka@gmail.com';}} className='footer__list-item-email'>
             Email
           </li>
+          <Link to={"https://www.youtube.com/playlist?list=PLnGqH8V263Zon4V1wSIbr5sjjW1_NHUHq"}>
           <li>
-
+            Youtube
           </li>
+          </Link>
+          <Link to={"https://www.instagram.com/tkafit/"}>
+          <li>
+            Instagram
+          </li>
+          </Link>
         </ul>
       </section>
+      <h3 className='footer__title'>Total Kinetic Advantage &copy;2024</h3>
 
     </div>
   )
